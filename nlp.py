a4aa696d4774d21cad6ec70a003603e073781de1
Neil <43568278+NeilTorrero@users.noreply.github.com>
2021-06-22 22:44:29 +0200

    Commented pretrained NeMo models

--- nlp.py
+++ nlp.py
@@ -9,3 +9,15 @@ from omegaconf import OmegaConf
 
 print(nemo_nlp.modules.get_pretrained_lm_models_list())
 nemo_nlp.modules.get_lm_model(pretrained_model_name="distilbert-base-uncased")
+
+# https://docs.nvidia.com/deeplearning/nemo/user-guide/docs/en/main/nlp/models.html
+# Intent Detection and slot tagging -> https://ngc.nvidia.com/catalog/collections/nvidia:tlt_jarvis_intentdetectionandslottagging
+#
+# Named Entity Recognition -> https://ngc.nvidia.com/catalog/collections/nvidia:tlt_jarvis_namedentityrecognition
+# pretrained("ner_en_bert")
+# Punctuation and capitalization -> https://ngc.nvidia.com/catalog/collections/nvidia:tlt_jarvis_punctuationandcapitalization
+# pretrained("punctuation_en_bert")
+# Question Answering -> https://ngc.nvidia.com/catalog/collections/nvidia:tlt_jarvis_questionanswering
+# pretrained("qa_squadv1.1_bertbase)
+# Text classification -> https://ngc.nvidia.com/catalog/collections/nvidia:tlt_jarvis_textclassification
+#
