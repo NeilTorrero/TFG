9f5864f8a6897d79ad30fc56e3282b11fb886b94
Neil <43568278+NeilTorrero@users.noreply.github.com>
2021-07-13 23:59:25 +0200

    Parts initial unification Simple Question-Response

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
+from rasa_api import check_Run_Server, check_Kill_Server, predictText, getResponses
+def chat():
+    """
+    Preprocess the input
+        - cleaning (stop words, uncase, spell check, etc)
+        - tokenize
+        - pos
+        - featurize (vector representation)
+        Intent Classification (from tokens or features)
+        Entity Extraction (from tokens or features)
+        - dependency parsing
+        - lemmatize
+        - ner
+        - nes/d and nel (synonym/disambiguation and linking)
+        Response selector
+    """
+    text = input("Your input: ")
+
+    prediction = predictText(text)
+    result = spacyPipeline(text)
+
+    intent = prediction['intent']['name']
+    print(intent)
+    if intent == 'inform':
+        print("Searching information.")
+    else:
+        response = getResponses(prediction)
+        print("Bot: ", response['messages']['text'])
+
+
 if __name__ == '__main__':
-    print("Hallo")
+    check_Run_Server()
+    chat()
+    check_Kill_Server()
--- rasa_api.py
+++ rasa_api.py
@@ -1,34 +1,67 @@
 import os
 import subprocess
 import sys
-
 import requests
-import signal
 
-response = ""
+
+def getResponses(prediction):
+    json_schema = '{"name": "' + prediction['intent']['name'] + '","entities": {'
+
+    first = True
+    for entity in prediction['entities']:
+        if not first:
+            json_schema += ','
+        else:
+            first = False
+        json_schema += '"' + entity['entity'] + '": "' + entity['value'] + '"'
+
+    json_schema += '}}'
+
+    response = requests.post("http://localhost:5005/conversations/default/trigger_intent", json_schema)
+    #print(response.json())
+    return response.json()
+
+def predictText(text):
+    response = requests.post("http://localhost:5005/model/parse",
+                             '{"text": "' + text + '"}')  # curl localhost:5005/model/parse -d '{"text":"I am mohit saini"}'
+    # print(response.json())
+
+    print(response.json()['intent_ranking'])
+    print(response.json()['entities'])
+
+    return response.json()
+
+
 api_run = ""
-try:
-    response = requests.get("http://localhost:5005/status")
-except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-    print("API Server Down")
-    print("run in shell the command rasa run --enable-api")
-    proj_dir = os.path.dirname(sys.modules['__main__'].__file__)
-    api_run = subprocess.Popen(['rasa', 'run', '--enable-api'], cwd=proj_dir + "/Rasa")
-
-    while True:
-        try:
-            response = requests.get("http://localhost:5005/status")
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            continue
-        print("API Server is now Up")
-        break
-except requests.exceptions.HTTPError:
-    print(" 4xx or 5xx")
-    exit()
-
-print("API Status:", response.status_code)
 
 
-if api_run != "":
-    api_run.kill()
-    print("API Server killed")
+def check_Run_Server():
+    global api_run
+    response = ""
+    try:
+        response = requests.get("http://localhost:5005/status")
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        print("API Server Down")
+        print("run in shell the command rasa run --enable-api")
+        proj_dir = os.path.dirname(sys.modules['__main__'].__file__)
+        api_run = subprocess.Popen(['rasa', 'run', '--enable-api'], cwd=proj_dir + "/Rasa")
+
+        while True:
+            try:
+                response = requests.get("http://localhost:5005/status")
+            except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+                continue
+            print("API Server is now Up")
+            break
+    except requests.exceptions.HTTPError:
+        print(" 4xx or 5xx")
+        exit()
+
+    print("API Status:", response.status_code)
+
+
+def check_Kill_Server():
+    global api_run
+    if api_run != "":
+        api_run.kill()
+        print("API Server killed")
