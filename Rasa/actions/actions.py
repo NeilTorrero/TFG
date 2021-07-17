2b5a14d1eb32976bb5c6c9e186f7a71c54789ae3
NeilTorrero <neil.torrero@students.salle.url.edu>
2021-07-17 19:30:00 +0200

    Functions need to be inside actions file

--- Rasa/actions/actions.py
+++ Rasa/actions/actions.py
@@ -12,7 +12,46 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-from backendCalls import getTime, getWeather
+import datetime
+import requests
+import pytz
+
+def searchInform(entities):
+    for entity in entities:
+        print(entity)
+
+
+def getTime(city):
+    # check for ip lookup timezone https://ipapi.com / https://apilayer.com/#products
+    # country_codes = {country: code for code, country in pytz.country_names.items()}
+    # print(pytz.country_timezones[country_codes['Germany']])
+    # tz = pytz.timezone('Europe/London')
+    now = datetime.datetime.now()#tz)
+    date = now.strftime("%A %dth of %B")
+    time = now.strftime("%H:%M")
+    return date, time
+
+
+def getWeather(city='Barcelona'):
+    # check for ip lookup city https://ipapi.com
+    API_KEY = "2e40656c78ebe1ec22f4f6a82540f208"
+    WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?"
+
+    url = WEATHER_URL + "q=" + city + "&appid=" + API_KEY + "&units=metric"
+
+    response = requests.get(url)
+    json_r = response.json()
+    if json_r['cod'] != '404':
+        info = json_r['main']
+        temperature = info['temp']
+        pressure = info['pressure']
+        humidity = info['humidity']
+        weather_description = json_r['weather'][0]['description']
+        print(temperature, pressure, humidity, weather_description)
+        return [temperature, pressure, humidity, weather_description]
+    else:
+        print("City not found")
+        return []
 
 
 class ActionAskTime(Action):
