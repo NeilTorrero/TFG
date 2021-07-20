71e68a201ed2c30af1ac8aac0871b99c9ee5cf06
Neil <43568278+NeilTorrero@users.noreply.github.com>
2021-07-21 00:51:45 +0200

    Google web scrapping working

--- backendCalls.py
+++ backendCalls.py
@@ -1,10 +1,79 @@
 import datetime
 import requests
 import pytz
+import urllib.parse
 
-def searchInform(entities):
-    for entity in entities:
-        print(entity)
+from bs4 import BeautifulSoup
+from requests_html import HTML
+from requests_html import HTMLSession
+
+
+def searchInform(question):
+    """question = "president of usa"
+    query = urllib.parse.quote_plus(question)
+
+    session = HTMLSession()
+    response = session.get("https://www.google.co.uk/search?q=" + query)
+
+    css_identifier_header = ".kp-header"#".ULSxyf"
+    css_identifier_result = ".tF2Cxc"
+    css_identifier_title = "h3"
+    css_identifier_link = ".yuRUbf a"
+    css_identifier_text = ".IsZvec"
+
+    header = response.html.find(css_identifier_header)
+    for result in header:
+        print(result.find(".FLP8od"))
+
+    results = response.html.find(css_identifier_result)
+
+    output = []
+
+    for result in results:
+        item = {
+            'title': result.find(css_identifier_title, first=True).text,
+            'link': result.find(css_identifier_link, first=True).attrs['href'],
+            'text': result.find(css_identifier_text, first=True).text
+        }
+
+        output.append(item)
+
+    print(output)"""
+
+    headers = {
+        'User-agent':
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582",
+    }
+
+    params = {
+        'hl': 'en',
+        'gl': 'us',
+        'lr': 'lang_en'
+    }
+
+    question = 'when was keanu reeves born'
+
+    html = requests.get('https://www.google.com/search?q=' + question + '&hl=en', headers=headers, params=params).text
+
+    soup = BeautifulSoup(html, 'lxml')
+
+    answer = soup.select_one('.Z0LcW.XcVN5d')
+    # if the box contains a upper link to de thing you are looking (USA / President)
+    # dates Z0LcW XcVN5d, people to but they might be in a link (<a>)
+    # for more info yxAsKe kZ91ed
+    more = soup.select_one('.yxAsKe.kZ91ed')
+    if answer is None:
+        # when info is inside divs '.zCubwf'
+        answera = soup.select_one('.zCubwf')
+        print(answera.text)
+    else:
+        print(answer.text)
+        print(more.text)
+
+    # when response is alone .IZ6rdc
+    # to get response from text which is in bold .hgKElc , b
+    # responses with a graph .KBXm4e
+
 
 
 def getTime(city):
@@ -12,7 +81,7 @@ def getTime(city):
     # country_codes = {country: code for code, country in pytz.country_names.items()}
     # print(pytz.country_timezones[country_codes['Germany']])
     # tz = pytz.timezone('Europe/London')
-    now = datetime.datetime.now()#tz)
+    now = datetime.datetime.now()  # tz)
     date = now.strftime("%A %dth of %B")
     time = now.strftime("%H:%M")
     return date, time
@@ -38,3 +107,7 @@ def getWeather(city='Barcelona'):
     else:
         print("City not found")
         return []
+
+
+if __name__ == '__main__':
+    searchInform("hello")
